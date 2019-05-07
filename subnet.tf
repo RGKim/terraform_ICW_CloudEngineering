faf38f815528fee399556db5c00bbaa07412d456
EC2 Default User <heehouse1@gmail.com>
2019-05-07 08:29:18 +0000

    illegal problem

--- subnet.tf
+++ subnet.tf
@@ -1,40 +1,40 @@
 resource "aws_subnet" "public_1a" {
-  vpc_id            = "${aws_vpc.dev.id}"
-  availability_zone = "ap-northeast-1a"
-  cidr_block        = "5.0.1.0/24"
+  vpc_id = "${aws_vpc.dev.id}"
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "5.0.1.0/24"
 
-  tags {
-    Name = "public-user05a"
-  }
+  tags {
+    Name = "public-user05a"
+    }
 }
 
 resource "aws_subnet" "private_1a" {
-  vpc_id            = "${aws_vpc.dev.id}"
-  availability_zone = "ap-northeast-1a"
-  cidr_block        = "5.0.101.0/24"
+    vpc_id = "${aws_vpc.dev.id}"
+    availability_zone = "ap-northeast-1a"
+    cidr_block = "5.0.101.0/24"
 
-  tags {
-    Name = "private-user05a"
-  }
+    tags {
+        Name = "private-user05a"
+    }
 }
-esource "aws_subnet" "public_1c" {
-  vpc_id            = "${aws_vpc.dev.id}"
-  availability_zone = "ap-northeast-1c"
-  cidr_block        = "5.0.2.0/24"
+resource "aws_subnet" "public_1c" {
+    vpc_id = "${aws_vpc.dev.id}"
+    availability_zone = "ap-northeast-1c"
+    cidr_block = "5.0.2.0/24"
 
-  tags {
-    Name = "public-user05c"
-  }
+    tags {
+        Name = "public-user05c"
+    }
 }
 
 resource "aws_subnet" "private_1c" {
-  vpc_id            = "${aws_vpc.dev.id}"
-  availability_zone = "ap-northeast-1c"
-  cidr_block        = "5.0.102.0/24"
+    vpc_id = "${aws_vpc.dev.id}"
+    availability_zone = "ap-northeast-1c"
+    cidr_block = "5.0.102.0/24"
 
-  tags {
-    Name = "private-user05c"
-  }
+    tags {
+        Name = "private-user05c"
+        }
 }
 
 
